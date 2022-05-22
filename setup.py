33222f9b25d2f6c227a8890e964386844fbe2801
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-22 22:34:27 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["WordToVec/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-WordToVec-Cy',
-    version='1.0.2',
+    version='1.0.3',
     packages=['WordToVec'],
     package_data={'WordToVec': ['*.pxd', '*.pyx', '*.c']},
     url='https://github.com/StarlangSoftware/WordToVec-Cy',
