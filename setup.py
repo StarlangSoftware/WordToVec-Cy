6a5bb931b9d9acb828539a280afeadc4147c764c
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2025-04-02 08:54:40 +0300

    Updated name and version.

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from Cython.Build import cythonize
 setup(
     ext_modules=cythonize(["WordToVec/*.pyx"],
                           compiler_directives={'language_level': "3"}),
-    name='NlpToolkit-WordToVec-Cy',
-    version='1.0.5',
+    name='nlptoolkit-wordtovec-cy',
+    version='1.0.6',
     packages=['WordToVec'],
     package_data={'WordToVec': ['*.pxd', '*.pyx', '*.c']},
     url='https://github.com/StarlangSoftware/WordToVec-Cy',
